e13e4bf842ef80d7574982cd178145ba5b77a4d9
Ben Kiboma <omayioben@gmail.com>
2022-05-29 20:46:48 +0300

    Redo arrow functions

--- scripts.js
+++ scripts.js
@@ -4,34 +4,25 @@ function message () {
 
 console.log(message());
 
+//Arrow function
 
-//Using the allow function
 let message2 = () => {
-  return "Hello World2!";
+  return "Hello world 2!"
 }
-
 console.log(message2());
 
-//We dont need curl brackets
-let message3 = () => "Hello World3!";
-
+//Arrow function without curl brackets
+let message3 = () => "Hello world 3!"
 console.log(message3());
 
-//Having a message inside the curl brackets
-let message4 = (name) => `Hello ${name}!`;
-
+//Arrow function with one argument
+let message4 = (name) => `Hello ${name}`
 console.log(message4("John"));
 
-//Removing the brackets
-let message5 = name => `Hello ${name}!`;
-
+//Arrow function with one argument remove brackets
+let message5 = name => `Hello ${name}`
 console.log(message5("John Mwai"));
 
-//Having two arguments
-let message6 = (name1,name2) => `${name1} and ${name2}`;
-
-console.log(message6("John","Ben"));
-
-//Multiplication
-let Multiplication = (a,b) => (a*b);
-document.getElementById("multiply").innerHTML=Multiplication(4,5);
+//Arrow function with two arguments
+let message6 = (name1,name2) => `${name1} and ${name2}`
+console.log(message6("John Mwai","Brian Kiplimo"));
